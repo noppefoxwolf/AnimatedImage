fbbb05e48501da4eb789caa2d7ee6d72eb95228d
noppe <noppelabs@gmail.com>
2024-12-04 01:51:55 +0900

    Update UIUpdateLink+.swift

--- Sources/UpdateLink/UIUpdateLink+.swift
+++ Sources/UpdateLink/UIUpdateLink+.swift
@@ -1,9 +1,9 @@
 import UIKit
 
-@available(iOS 18.0, *)
+@available(iOS 18.0, visionOS 2.0, *)
 extension UIUpdateInfo: UpdateInfo {}
 
-@available(iOS 18.0, *)
+@available(iOS 18.0, visionOS 2.0, *)
 extension UIUpdateLink: UpdateLink {
     public func addAction(
         handler: @escaping (any UpdateLink, any UpdateInfo) -> Void
