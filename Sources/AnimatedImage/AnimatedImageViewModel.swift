5dbc7f51b978eb86ebca5122daeeaa7c17d194e7
noppe <noppelabs@gmail.com>
2025-08-18 10:38:06 +0900

    Move AnimatedImageViewConfiguration to separate file
    Extracted AnimatedImageViewConfiguration from AnimatedImageViewModel.swift into its own file for better modularity and code organization.

--- Sources/AnimatedImage/AnimatedImageViewModel.swift
+++ Sources/AnimatedImage/AnimatedImageViewModel.swift
@@ -6,48 +6,6 @@ fileprivate let logger = Logger(
     category: #file
 )
 
-public struct AnimatedImageViewConfiguration: Sendable {
-    public static var unlimited: AnimatedImageViewConfiguration {
-        AnimatedImageViewConfiguration(
-            maxByteCount: .max,
-            maxSize: CGSize(width: Double.infinity, height: Double.infinity),
-            maxLevelOfIntegrity: 1,
-            interpolationQuality: .high,
-            contentsFilter: .trilinear,
-            taskPriority: .userInitiated
-        )
-    }
-    
-    public static var `default`: AnimatedImageViewConfiguration {
-        AnimatedImageViewConfiguration(
-            maxByteCount: 1 * 1024 * 1024, // 1MB
-            maxSize: CGSize(width: 128, height: 128),
-            maxLevelOfIntegrity: 0.8,
-            interpolationQuality: .default,
-            contentsFilter: .linear,
-            taskPriority: .medium
-        )
-    }
-    
-    public static var performance: AnimatedImageViewConfiguration {
-        AnimatedImageViewConfiguration(
-            maxByteCount: 1 * 1024 * 1024 / 50, // 20KB
-            maxSize: CGSize(width: 32, height: 32),
-            maxLevelOfIntegrity: 0.25,
-            interpolationQuality: .none,
-            contentsFilter: .nearest,
-            taskPriority: .low
-        )
-    }
-    
-    public var maxByteCount: Int64
-    public var maxSize: CGSize
-    public var maxLevelOfIntegrity: Double
-    public var interpolationQuality: CGInterpolationQuality
-    public var contentsFilter: CALayerContentsFilter
-    public var taskPriority: TaskPriority
-}
-
 @MainActor
 internal final class AnimatedImageViewModel: Sendable {
     enum CacheKey: Hashable {
