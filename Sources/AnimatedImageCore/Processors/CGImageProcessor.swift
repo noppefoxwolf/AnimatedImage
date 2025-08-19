cd0958af34122e3aed8837a18ba9e1580040769a
noppe <noppelabs@gmail.com>
2025-08-19 20:33:06 +0900

    Add gray PNG demo and update image processor
    Added a new gray PNG image resource and demo section in FormatDemoView. Updated CGImageProcessor to use device RGB color space and improved bitmapInfo handling. Removed unnecessary print statement from SwiftUIDemoView.

--- Sources/AnimatedImageCore/Processors/CGImageProcessor.swift
+++ Sources/AnimatedImageCore/Processors/CGImageProcessor.swift
@@ -28,21 +28,20 @@ public actor CGImageProcessor: Sendable {
 
         guard width > 0 && height > 0 else { return nil }
 
-        let colorSpace = image.colorSpace ?? CGColorSpace(name: CGColorSpace.sRGB)!
-        let bitmapInfo =
-            CGBitmapInfo.byteOrder32Little.rawValue | CGImageAlphaInfo.premultipliedFirst.rawValue
+        let colorSpace = CGColorSpaceCreateDeviceRGB()
+        let bitmapInfo: CGBitmapInfo = [.byteOrder32Little, CGBitmapInfo(rawValue: CGImageAlphaInfo.premultipliedFirst.rawValue)]
 
-        guard
-            let context = CGContext(
-                data: nil,
-                width: width,
-                height: height,
-                bitsPerComponent: 8,
-                bytesPerRow: width * 4,
-                space: colorSpace,
-                bitmapInfo: bitmapInfo
-            )
-        else { return nil }
+        let context = CGContext(
+            data: nil,
+            width: width,
+            height: height,
+            bitsPerComponent: 8,
+            bytesPerRow: width * 4,
+            space: colorSpace,
+            bitmapInfo: bitmapInfo.rawValue
+        )
+        
+        guard let context else { fatalError() }
 
         context.interpolationQuality = interpolationQuality
         context.draw(image, in: CGRect(origin: .zero, size: newSize.cgSize))
